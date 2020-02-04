7419e11e5cdc695344d0309bd057de2d8788d915
lucasmation <lucasmation@gmail.com>
2020-02-03 20:21:10 -0600

    update train_clean, add test_clean, small fixes to code

--- prediction_in_R.R
+++ prediction_in_R.R
@@ -3,6 +3,9 @@ library(stringr)
 library(readxl)
 library(data.table)
 library(caret)
+library(dvmisc)
+library(rsq)
+library(boot)
 
 
 names(d) %>% tail
@@ -66,6 +69,9 @@ d <- d[!is.na(drinks)]
 
 
 #new variables
+d[,female:=(gender==2) %>% as.numeric]
+T[,female:=(gender==2) %>% as.numeric]
+
 d[,age:=2015-birthyear]
 T[,age:=2015-birthyear]
   d[,.(drinks,inc_fam1997)][, lapply(.SD, encode_NA)][,.N,inc_fam1997][order(-N)]
@@ -78,6 +84,7 @@ d[age==34, inc_fam_16t22:=rowMeans(.SD,na.rm = TRUE), .SDcols = c('inc_fam2000',
 d[age==35, inc_fam_16t22:=rowMeans(.SD,na.rm = TRUE), .SDcols = c('inc_fam2001','inc_fam2002','inc_fam2003','inc_fam2004','inc_fam2005','inc_fam2006','inc_fam2007')]
 d[,inc_fam_16t22_MI:=inc_fam_16t22 %>% is.na %>% as.numeric]
 d[,inc_fam_16t22   :=inc_fam_16t22 %>% NA2mean]
+  #d[,.N,inc_fam_16t22][order(-N)]
   #d[,.N,inc_fam_16t22_MI]
   #d[age==31, .(inc_fam_16t22,inc_fam_16t22_MI,inc_fam1997,inc_fam1998,inc_fam1999,inc_fam2000,inc_fam2001,inc_fam2002,inc_fam2003)] %>% View
 
@@ -96,6 +103,7 @@ T[,inc_fam_16t22   :=inc_fam_16t22 %>% NA2mean]
 d[, inc_fam_2010t2015:=rowMeans(.SD,na.rm = TRUE), .SDcols = c('inc_fam2010','inc_fam2011','inc_fam2013','inc_fam2015')]
 d[,inc_fam_2010t2015_MI:=inc_fam_2010t2015 %>% is.na %>% as.numeric]
 d[,inc_fam_2010t2015   :=inc_fam_2010t2015 %>% NA2mean]
+  #d[,.N,inc_fam_2010t2015][order(-N)]
   #d[,.N,inc_fam_2010t2015_MI]
   #d[,.(inc_fam_2010t2015,inc_fam_2010t2015_MI,inc_fam2010,inc_fam2011,inc_fam2013,inc_fam2015)]
 
@@ -106,24 +114,30 @@ T[,inc_fam_2010t2015   :=inc_fam_2010t2015 %>% NA2mean]
 
 
 
-#Exporting datasets:
-d %>% fwrite(fo1)
-T %>% fwrite(fo2)
-
-
-names(dJ)
-
-#Dealing with  missings:
 
 
 #Integrating Juan's data:
 
 dJ <- fread(f1J)
 dJ[,V1:=NULL]
+dJ <- dJ[health_status_2015>=0]
 names(dJ)
+dJ[,.N,ave_pov_2010t2015][order(-N)]
+dJ[,ave_pov_2010t2015_MI:=ave_pov_2010t2015 %>% is.na %>% as.numeric]
+dJ[,ave_pov_2010t2015   :=ave_pov_2010t2015 %>% NA2mean]
+
+
 TJ <- fread(f2J)
+TJ[,V1:=NULL]
+names(TJ)
+TJ[,.N,ave_pov_2010t2015][order(-N)]
+TJ[,ave_pov_2010t2015_MI:=ave_pov_2010t2015 %>% is.na %>% as.numeric]
+TJ[,ave_pov_2010t2015   :=ave_pov_2010t2015 %>% NA2mean]
+
+
 
 dF <- d[,.(diag.id,drinks,age,female,race,inc_fam_16t22, inc_fam_16t22_MI, inc_fam_2010t2015, inc_fam_2010t2015_MI)]
+TF <- T[,.(diag.id,       age,female,race,inc_fam_16t22, inc_fam_16t22_MI, inc_fam_2010t2015, inc_fam_2010t2015_MI)]
 
 dF <- dF[dJ,on='diag.id']
 TF <- TF[TJ,on='diag.id']
@@ -132,7 +146,7 @@ TF <- TF[TJ,on='diag.id']
 
 #Exporting clean version
 dF %>% fwrite(fo1)
-T %>% fwrite(fo2)
+TF %>% fwrite(fo2)
 
 
 
@@ -147,7 +161,6 @@ d[,.N,age]
 d[,.N,race]
 d[,.N,gender]
 d[,.N,female]
-d[,female:=(gender==2) %>% as.numeric]
 
 
 d[drinks>0] %>% lm(drinks ~ race + gender + age,.) %>% summary %>% .$adj.r.squared
@@ -156,3 +169,58 @@ d[drinks>0] %>% lm(drinks ~ race + gender + age,.) %>% summary %>% .$adj.r.squar
 
 control <- trainControl(method="cv", number=10)
 metric <- "Accuracy"
+
+
+
+dF %>% lm(drinks ~ age + factor(race) + female,.) %>% summary %>% .$r.squared
+dF %>% lm(drinks ~ 
+            age + factor(race) + female + 
+            inc_fam_16t22 + inc_fam_16t22_MI + inc_fam_2010t2015 + inc_fam_2010t2015_MI + 
+            ave_hh_size_2010t2015 + ave_pov_2010t2015 + ave_totwork_h + 
+            kids6_2015 + ave_kids6 + kids6_mi + nkids_mi + factor(marriage_status) +
+            factor(health_status_2015),.) %>% 
+  summary %>% .$adj.r.squared
+
+mse <- function(x) mean(x^2)
+dF %>% lm(drinks ~ 
+            age + factor(race) + female + 
+            inc_fam_16t22 + inc_fam_16t22_MI + inc_fam_2010t2015 + inc_fam_2010t2015_MI + 
+            ave_hh_size_2010t2015 + ave_pov_2010t2015 + ave_totwork_h + 
+            kids6_2015 + ave_kids6 + kids6_mi + nkids_mi + factor(marriage_status) +
+            factor(health_status_2015),.) %>% 
+  get_mse( var.estimate = FALSE)
+  summary %>% .$residuals %>% mse
+  
+  
+poisson.model <- dF %>% 
+      glm(drinks ~ 
+            age + factor(race) + female + 
+            inc_fam_16t22 + inc_fam_16t22_MI + inc_fam_2010t2015 + inc_fam_2010t2015_MI + 
+            ave_hh_size_2010t2015 + ave_pov_2010t2015 + ave_totwork_h + 
+            kids6_2015 + ave_kids6 + kids6_mi + nkids_mi + factor(marriage_status) +
+            factor(health_status_2015),
+          .,family="poisson")
+poisson.model %>% rsq(adj=TRUE)
+poisson.model %>% get_mse( var.estimate = FALSE)
+
+cv.err.10 <- dF %>% cv.glm(poisson.model,K=10)
+
+muhat <- poisson.model$fitted
+poisson.diag <- glm.diag(poisson.model)
+mean((poisson.model$y-muhat)^2/(1-poisson.diag$h)^2)
+
+dF %>% cv.glm(poisson.model,K=10)
+
+  
+summary %>% .$residuals %>% mse
+
+
+install.packages('rsq')
+
+
+glm(Monarchs ~ Garden,
+    data=Data,
+    family="poisson")
+glm
+
+
